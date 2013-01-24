fe89feaa5a71d0b2374ca4b60aa0713acff810dd
mode89 <mode.andrew@gmail.com>
2013-01-25 00:12:33 +0400

    + Caching path to VCVARSALL.BAT

--- build-test.bat
+++ build-test.bat
@@ -1,9 +1,5 @@
 @echo off
-
-rem Setup Visual Studio environment
-
-for /F "delims=," %%F in ('dir /S /B C:\*vcvarsall.bat') do set VCVARSALL_BAT=%%F
-call "%VCVARSALL_BAT%"
+set START_DIR=%CD%
 
 rem Make build directory
 
@@ -20,6 +16,23 @@ mingw32-make
 
 cd ..
 
+rem Setup Visual Studio environment
+
+if not exist vcvarsall.bat (
+    for /F "delims=," %%F in ('dir /S /B C:\*vcvarsall.bat') do ( set VCVARSALL_BAT=%%F )
+)
+
+if not exist vcvarsall.bat (
+    @if not "%VCVARSALL_BAT%"=="" (
+        echo call "%VCVARSALL_BAT%" > vcvarsall.bat
+    ) else (
+        echo Cannot locate vcvarsval.bat
+        goto end
+    )
+)
+
+call vcvarsall.bat
+
 rem Build release version with Visual Studio
 
 if not exist msvc-release ( mkdir msvc-release )
@@ -27,5 +40,8 @@ cd msvc-release
 
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ../..
 nmake
+
+:end
+cd %START_DIR%
 
 pause
