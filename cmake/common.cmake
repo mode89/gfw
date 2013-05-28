dbf5aa7060bf21437163417083afe287f1330627
mode89 <mode.andrew@gmail.com>
2013-05-29 01:23:10 +0400

    GLOBAL * Moved common CMake-code to cmake/common.cmake

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,6 +1,16 @@
 project(CMAKE)
 cmake_minimum_required(VERSION 2.8)
 
+# Setup common packages search path
+
+set(CMAKE_PREFIX_PATH ${CMAKE_CURRENT_SOURCE_DIR})
+
+# Enable multithreaded compiling
+
+if (MSVC)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
+endif()
+
 macro(target_precompiled_header target precHeader precSource)
     if (MSVC)
         set_target_properties(${target} PROPERTIES COMPILE_FLAGS "/Yu${precHeader}")
