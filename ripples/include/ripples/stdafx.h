57430e328bd8c8ea2de5424ee8e94fa7284571f8
mode89 <mode.andrew@gmail.com>
2013-06-01 03:42:23 +0400

    RIPPLES + Initial files. Draw a quad

---
+++ ripples/include/ripples/stdafx.h
@@ -0,0 +1,10 @@
+#ifndef __RIPPLES_STDAFX_H__
+#define __RIPPLES_STDAFX_H__
+
+#include "OGRE/OgreRoot.h"
+#include "OGRE/OgreRenderSystem.h"
+#include "OGRE/OgreRenderWindow.h"
+#include "OGRE/OgreWindowEventUtilities.h"
+#include "OGRE/OgreManualObject.h"
+
+#endif // __RIPPLES_STDAFX_H__
---
+++ ripples/source/stdafx.cpp
@@ -0,0 +1,1 @@
+#include "ripples/stdafx.h"
