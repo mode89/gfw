550021266f9f8e4e0adbde37c469fdd8954afc71
mode89 <mode89@mail.ru>
2013-07-28 04:51:34 +0400

    RIPPLES + Clear window

--- ripples/source/main.cpp
+++ ripples/source/main.cpp
@@ -1,20 +1,47 @@
+#include "common/trace.h"
+
 #include "gfw/gfw.h"
 
 using namespace GFW;
 
+static const uint32_t kWindowWidth  = 800;
+static const uint32_t kWindowHeight = 600;
+
 int main()
 {
+    WindowDesc windowDesc;
+    windowDesc.width  = kWindowWidth;
+    windowDesc.height = kWindowHeight;
+    WindowHandle window = CreateDefaultWindow(windowDesc);
+
     DeviceParams deviceParams;
-    deviceParams.width          = 800;
-    deviceParams.height         = 600;
-    deviceParams.windowHandle   = NULL; // Force to create a default window
+    deviceParams.width          = kWindowWidth;
+    deviceParams.height         = kWindowHeight;
+    deviceParams.windowHandle   = window;
 
     IDeviceRef device = CreateDevice(deviceParams);
 
-    do 
-    {
+    IContextRef context = device->GetDefaultContext();
 
-    } while (device->Present());
+    while (ProcessDefaultWindow(window))
+    {
+        context->BeginScene();
+        {
+            static ClearParams clearParams;
+            clearParams.mask  = CLEAR_COLOR;
+            clearParams.color[0] = 0.0f;
+            clearParams.color[1] = 0.0f;
+            clearParams.color[2] = 0.3f;
+            clearParams.color[3] = 1.0f;
+
+            context->Clear(clearParams);
+        }
+        context->EndScene();
+
+        device->Present();
+    }
+
+    DestroyDefaultWindow(window);
 
     return 0;
 }
