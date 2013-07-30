92d7eb077c2f94473b4a716bd7ea04d55b854de1
mode89 <mode89@mail.ru>
2013-07-31 03:36:57 +0400

    RIPPLES + Initialization of the mesh

--- ripples/source/main.cpp
+++ ripples/source/main.cpp
@@ -2,13 +2,38 @@
 
 #include "gfw/gfw.h"
 
+#include <math.h>
+
+using namespace Common;
 using namespace GFW;
 
 static const uint32_t kWindowWidth  = 800;
 static const uint32_t kWindowHeight = 600;
 
+static const uint32_t kMaxMeshNodeCount = 1000;
+
 int main()
 {
+    // Initialize the mesh
+
+    uint32_t xNodeDim = static_cast<uint32_t>(sqrtf(kMaxMeshNodeCount * kWindowWidth / kWindowHeight));
+    uint32_t yNodeDim = kMaxMeshNodeCount / xNodeDim;
+    uint32_t nodeCnt  = xNodeDim * yNodeDim;
+
+    TRACE_MESSAGE_FORMATTED("Node count: %d\n", nodeCnt);
+
+    AutoPointer<float> f = new float [nodeCnt];
+    for (uint32_t j = 0; j < yNodeDim; ++ j)
+    {
+        uint32_t offset = j * xNodeDim;
+        for (uint32_t i = 0; i < xNodeDim; ++ i)
+        {
+            f[i + offset] = 0.0f;
+        }
+    }
+
+    // Initialize graphics context
+
     WindowDesc windowDesc;
     windowDesc.width  = kWindowWidth;
     windowDesc.height = kWindowHeight;
@@ -35,6 +60,9 @@ int main()
             clearParams.color[3] = 1.0f;
 
             context->Clear(clearParams);
+
+            // TODO Calculate the mesh
+            // TODO Draw the mesh
         }
         context->EndScene();
 
