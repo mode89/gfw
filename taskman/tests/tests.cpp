64dda4f24b5869ddadc421213383271c040e0167
mode89 <mode.andrew@gmail.com>
2013-09-06 21:59:20 +0400

    TASKMAN * Refactored test

--- taskman/tests/tests.cpp
+++ taskman/tests/tests.cpp
@@ -3,40 +3,53 @@
 #include "common/futex.h"
 #include "taskman/taskman.h"
 
-#define TASK_COUNT  1000000
-#define ITER_COUNT  10000
+#define TASK_COUNT          1000000
+#define TASK_ITER_COUNT     1000
 
 using namespace TaskMan;
+
+struct TaskData
+{
+    uint64_t *          totalSum;
+    Common::Futex *     mutex;
+};
 
 void Task(void * data)
 {
     uint32_t sum = 0;
-    for (int i = 0; i < ITER_COUNT; ++ i)
+
+    for (int i = 0; i < TASK_ITER_COUNT; ++ i)
     {
         sum += i;
     }
 
-    static Common::Futex mutexData;
-    mutexData.Lock();
-    (*reinterpret_cast<uint64_t*>(data)) += sum;
-    mutexData.Unlock();
+    TaskData * taskData = reinterpret_cast<TaskData*>(data);
+
+    taskData->mutex->Lock();
+    *taskData->totalSum += sum;
+    taskData->mutex->Unlock();
 }
 
 TEST(TaskManagerTests, CreateAndRunTasks)
 {
     ITaskManagerRef taskManager = TaskMan::GetInstance();
 
-    ITaskRef * tasks = new ITaskRef [TASK_COUNT];
+    Common::AutoPointer<ITaskRef> tasks    = new ITaskRef [TASK_COUNT];
+    Common::AutoPointer<TaskData> taskData = new TaskData [TASK_COUNT];
 
+    Common::Futex mutexSum;
     uint64_t sum = 0;
 
     for (int i = 0; i < TASK_COUNT; ++ i)
     {
+        taskData[i].mutex = &mutexSum;
+        taskData[i].totalSum = &sum;
+
         tasks[i] = taskManager->CreateTask(Task);
-        tasks[i]->Run(&sum);
+        tasks[i]->Run(&taskData[i]);
     }
 
     taskManager->Run();
 
-    ASSERT_TRUE(sum == (1ll * TASK_COUNT * (0 + (ITER_COUNT - 1)) * ITER_COUNT / 2));
+    ASSERT_TRUE(sum == (1ll * TASK_COUNT * (0 + (TASK_ITER_COUNT - 1)) * TASK_ITER_COUNT / 2));
 }
