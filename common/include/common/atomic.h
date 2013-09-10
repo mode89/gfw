bc375a209ccb1556f1cc2719387d5efcdd9b5039
mode89 <mode.andrew@gmail.com>
2013-09-10 14:05:44 +0400

    COMMON + Atomic exchange & compare-exchange

--- common/include/common/atomic.h
+++ common/include/common/atomic.h
@@ -7,6 +7,8 @@ namespace Common {
 
     void AtomicIncrement(int32_t &);
     void AtomicDecrement(int32_t &);
+    void AtomicExchange(int32_t & dst, int32_t val);
+    void AtomicCompareExchange(int32_t & dst, int32_t exch, int32_t cmp);
 
 } // namespace Common
 
--- common/source/atomic.cpp
+++ common/source/atomic.cpp
@@ -7,16 +7,28 @@
 
     namespace Common {
 
-        void AtomicIncrement(int32_t & val)
+        void AtomicIncrement(int32_t & dst)
         {
-            volatile LONG * v = static_cast<volatile LONG *>(static_cast<void *>(&val));
-            InterlockedIncrement(v);
+            volatile LONG * d = static_cast<volatile LONG *>(static_cast<void *>(&dst));
+            InterlockedIncrement(d);
         }
 
-        void AtomicDecrement(int32_t & val)
+        void AtomicDecrement(int32_t & dst)
         {
-            volatile LONG * v = static_cast<volatile LONG *>(static_cast<void *>(&val));
-            InterlockedDecrement(v);
+            volatile LONG * d = static_cast<volatile LONG *>(static_cast<void *>(&dst));
+            InterlockedDecrement(d);
+        }
+
+        void AtomicExchange(int32_t & dst, int32_t val)
+        {
+            volatile LONG * d = static_cast<volatile LONG *>(static_cast<void *>(&dst));
+            InterlockedExchange(d, val);
+        }
+
+        void AtomicCompareExchange(int32_t & dst, int32_t exch, int32_t cmp)
+        {
+            volatile LONG * d = static_cast<volatile LONG *>(static_cast<void *>(&dst));
+            InterlockedCompareExchange(d, exch, cmp);
         }
 
     } // namespace Common
