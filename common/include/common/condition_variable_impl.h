9cc799092b3779e9d29933eba48fc34db847436b
mode89 <mode89@mail.ru>
2013-09-12 03:22:43 +0400

    COMMON + MinGW implementation of the condition variable

--- common/include/common/condition_variable_impl.h
+++ common/include/common/condition_variable_impl.h
@@ -1,6 +1,11 @@
 #ifndef __COMMON_CONDITION_VARIABLE_IMPL_H__
 #define __COMMON_CONDITION_VARIABLE_IMPL_H__
 
+#include "common/platform.h"
+#include "common/typedefs.h"
+
+#ifdef PLAT_COMPILER_MSVC
+
 #include <windows.h>
 
 namespace Common {
@@ -11,5 +16,24 @@ namespace Common {
     };
 
 } // namespace Common
+
+#elif defined(PLAT_COMPILER_MINGW)
+
+#include <windows.h>
+
+namespace Common {
+
+    struct ConditionVariableImpl
+    {
+        HANDLE              nativeHandle;
+        uint32_t            waitersCount;
+        CRITICAL_SECTION    mutexWaitersCount;
+    };
+
+} // namespace Common
+
+#else
+#error Undefined platform
+#endif
 
 #endif // __COMMON_CONDITION_VARIABLE_IMPL_H__
--- common/source/condition_variable.cpp
+++ common/source/condition_variable.cpp
@@ -2,7 +2,7 @@
 #include "common/condition_variable_impl.h"
 #include "common/mutex_impl.h"
 
-#include <windows.h>
+#ifdef PLAT_COMPILER_MSVC
 
 namespace Common {
 
@@ -29,3 +29,60 @@ namespace Common {
     }
 
 } // namespace Common
+
+#elif defined(PLAT_COMPILER_MINGW)
+
+namespace Common {
+
+    ConditionVariable::ConditionVariable()
+        : mImpl(NULL)
+    {
+        mImpl = new ConditionVariableImpl;
+        ZeroMemory(mImpl, sizeof(ConditionVariableImpl));
+
+        mImpl->nativeHandle = CreateEvent(NULL, TRUE, FALSE, NULL);
+        InitializeCriticalSection(&mImpl->mutexWaitersCount);
+        mImpl->waitersCount = 0;
+    }
+
+    ConditionVariable::~ConditionVariable()
+    {
+        CloseHandle(mImpl->nativeHandle);
+        DeleteCriticalSection(&mImpl->mutexWaitersCount);
+        delete mImpl;
+    }
+
+    void ConditionVariable::Wait(Mutex & mutex)
+    {
+        EnterCriticalSection(&mImpl->mutexWaitersCount);
+        mImpl->waitersCount ++;
+        LeaveCriticalSection(&mImpl->mutexWaitersCount);
+
+        LeaveCriticalSection(&mutex.mImpl->nativeHandle);
+        WaitForSingleObject(mImpl->nativeHandle, INFINITE);
+        EnterCriticalSection(&mutex.mImpl->nativeHandle);
+
+        EnterCriticalSection(&mImpl->mutexWaitersCount);
+        mImpl->waitersCount --;
+        if (mImpl->waitersCount == 0)
+        {
+            ResetEvent(mImpl->nativeHandle);
+        }
+        LeaveCriticalSection(&mImpl->mutexWaitersCount);
+    }
+
+    void ConditionVariable::Notify()
+    {
+        EnterCriticalSection(&mImpl->mutexWaitersCount);
+        if (mImpl->waitersCount > 0)
+        {
+            SetEvent(mImpl->nativeHandle);
+        }
+        LeaveCriticalSection(&mImpl->mutexWaitersCount);
+    }
+
+} // namespace Common
+
+#else
+#error Undefined platform
+#endif
