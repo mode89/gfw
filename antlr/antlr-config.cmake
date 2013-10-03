6592396df477ad097ef5ecec51642de213209139
mode89 <mode89@mail.ru>
2013-10-04 03:01:18 +0400

    ANTLR + antlr_prebuild_grammar()

--- antlr/antlr-config.cmake
+++ antlr/antlr-config.cmake
@@ -2,3 +2,8 @@ list( APPEND ANTLR_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/include )
 list( APPEND ANTLR_INCLUDE_DIRS ${CMAKE_CURRENT_LIST_DIR}/include/antlr )
 set( ANTLR_LIBRARIES antlr )
 set( ANTLR_TOOL ${CMAKE_CURRENT_LIST_DIR}/bin/antlr-3.4-complete-no-antlrv2.jar )
+
+function( antlr_prebuild_grammar target grammar )
+    add_custom_command( TARGET ${target} PRE_BUILD COMMAND
+        java ARGS -jar ${ANTLR_TOOL} -make -language C ${grammar} )
+endfunction()
