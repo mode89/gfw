53e83b005b64182ca5ac21a1f954b46b171f9603
mode89 <mode89@mail.ru>
2013-11-01 03:22:03 +0400

    GFW FXC + Parsed -fx, -o command-line options

--- gfw/source/fxc/main.cpp
+++ gfw/source/fxc/main.cpp
@@ -1,4 +1,70 @@
+#include "common/trace.h"
+
+#include <cstring>
+#include <iostream>
+#include <string>
+
+void PrintHelp()
+{
+    std::cout
+        << "Usage:" << std::endl
+        << std::endl
+        << "gfw_fxc [options]" << std::endl
+        << std::endl
+        << "Options:" << std::endl
+        << std::endl
+        << "-fx <filename>      Path to the FX file" << std::endl
+        << "-o <filename>       Path to the compiled effect" << std::endl
+        << std::endl;
+}
+
 int main( int argc, const char * argv[] )
 {
+    if ( argc < 2 )
+    {
+        PrintHelp();
+        return -1;
+    }
+
+    std::string fxFile;
+    std::string outputFile;
+
+    for ( int i = 1; i < argc; ++ i )
+    {
+        const char * arg = argv[i];
+        if ( std::strcmp( "-fx", arg ) == 0 )
+        {
+            if ( ++i < argc )
+            {
+                fxFile = argv[i];
+            }
+            else
+            {
+                TRACE_ERROR( "Missed filename after -fx option" );
+                PrintHelp();
+                return -1;
+            }
+        }
+        else if ( std::strcmp( "-o", arg ) == 0 )
+        {
+            if ( ++i < argc )
+            {
+                outputFile = argv[i];
+            }
+            else
+            {
+                TRACE_ERROR( "Missed filename after -o option" );
+                PrintHelp();
+                return -1;
+            }
+        }
+        else
+        {
+            TRACE_ERROR_FORMATTED( "Unknown command line argument '%s'", arg );
+            PrintHelp();
+            return -1;
+        }
+    }
+
     return 0;
 }
