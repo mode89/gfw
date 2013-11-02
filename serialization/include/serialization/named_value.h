e375969379a9758408381bb423cef59460e12ddb
mode89 <mode89@mail.ru>
2013-11-03 02:39:31 +0400

    SERIALIZATION + NamedValue

---
+++ serialization/include/serialization/named_value.h
@@ -0,0 +1,28 @@
+#ifndef __SERIALIZATION_NAMED_VALUE_H__
+#define __SERIALIZATION_NAMED_VALUE_H__
+
+namespace Serialization {
+
+    template < class T >
+    class NamedValue
+    {
+    public:
+        NamedValue( const char * name, T & value )
+            : mName( name )
+            , mValue( value )
+        {}
+
+    private:
+        const char *    mName;
+        T &             mValue;
+    };
+
+    template < class T >
+    NamedValue<T> CreateNamedValue( const char * name, T & value )
+    {
+        return NamedValue<T>( name, value );
+    }
+
+} // namespace Serialization
+
+#endif // __SERIALIZATION_NAMED_VALUE_H__
