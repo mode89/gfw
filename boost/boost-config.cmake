84ea49b4d1bdb0a02104c2a00bc3210034d6cf20
mode <mode.andrew@gmail.com>
2014-07-22 15:58:37 +0400

    boost + building boost-serialization

--- boost/boost-config.cmake
+++ boost/boost-config.cmake
@@ -1,6 +1,12 @@
 file( TO_CMAKE_PATH $ENV{BOOST_ROOT} BOOST_ROOT )
+
 set( BOOST_INCLUDE_DIRS ${BOOST_ROOT} )
-set( BOOST_LIBS boost-wave )
+
+set( BOOST_LIBS
+    boost-wave
+    boost-serialization
+    )
+
 set( BOOST_DEFINITIONS
     -DBOOST_ALL_NO_LIB # Prevents automatic linking
     )
