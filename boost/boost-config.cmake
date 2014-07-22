e40872f84bfa755ea511165b157f7983b24a5cba
mode <mode.andrew@gmail.com>
2014-07-22 15:55:52 +0400

    boost + preventing auto-linking

--- boost/boost-config.cmake
+++ boost/boost-config.cmake
@@ -1,3 +1,6 @@
 file( TO_CMAKE_PATH $ENV{BOOST_ROOT} BOOST_ROOT )
 set( BOOST_INCLUDE_DIRS ${BOOST_ROOT} )
 set( BOOST_LIBS boost-wave )
+set( BOOST_DEFINITIONS
+    -DBOOST_ALL_NO_LIB # Prevents automatic linking
+    )
