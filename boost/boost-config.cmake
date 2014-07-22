16a649e98e60c4583a1de9a969bcaf040c47c8d3
mode <mode.andrew@gmail.com>
2014-07-22 21:10:10 +0400

    boost + building boost-filesystem, boost-system

--- boost/boost-config.cmake
+++ boost/boost-config.cmake
@@ -3,8 +3,10 @@ file( TO_CMAKE_PATH $ENV{BOOST_ROOT} BOOST_ROOT )
 set( BOOST_INCLUDE_DIRS ${BOOST_ROOT} )
 
 set( BOOST_LIBS
-    boost-wave
+    boost-filesystem
     boost-serialization
+    boost-system
+    boost-wave
     )
 
 set( BOOST_DEFINITIONS
