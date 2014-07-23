3d1449c1d42eb4a87e89d28d1d14042bc50a640a
mode89 <mode89@mail.ru>
2014-07-23 17:23:15 +0400

    boost + boost-config.cmake : throwing error if BOOST_ROOT not defined

--- boost/boost-config.cmake
+++ boost/boost-config.cmake
@@ -1,14 +1,18 @@
-file( TO_CMAKE_PATH $ENV{BOOST_ROOT} BOOST_ROOT )
+if ( "$ENV{BOOST_ROOT}" STREQUAL "" )
+    message( SEND_ERROR "Failed to find Boost. Environment variable BOOST_ROOT is undefined." )
+else()
+    file( TO_CMAKE_PATH $ENV{BOOST_ROOT} BOOST_ROOT )
 
-set( BOOST_INCLUDE_DIRS ${BOOST_ROOT} )
+    set( BOOST_INCLUDE_DIRS ${BOOST_ROOT} )
 
-set( BOOST_LIBS
-    boost-filesystem
-    boost-serialization
-    boost-system
-    boost-wave
-    )
+    set( BOOST_LIBS
+        boost-filesystem
+        boost-serialization
+        boost-system
+        boost-wave
+        )
 
-set( BOOST_DEFINITIONS
-    -DBOOST_ALL_NO_LIB # Prevents automatic linking
-    )
+    set( BOOST_DEFINITIONS
+        -DBOOST_ALL_NO_LIB # Prevents automatic linking
+        )
+endif()
