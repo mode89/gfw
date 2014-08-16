1858250d62127699eac80c0eecd82fb7712e0114
mode <mode.andrew@gmail.com>
2014-08-16 16:41:22 +0400

    gfw tests runtime + GraphicsTest::MapWriteDynamicTexture

--- gfw/tests/runtime/texture.cpp
+++ gfw/tests/runtime/texture.cpp
@@ -51,3 +51,59 @@ TEST_F( GraphicsTest, Texture )
         Present();
     }
 }
+
+TEST_F( GraphicsTest, MapWriteDynamicTexture )
+{
+    static const uint32_t   kTextureWidth   = 64;
+    static const Format     kTextureFormat  = FORMAT_RGBA8_UNORM;
+
+    TextureDesc textureDesc;
+    textureDesc.usage           = USAGE_DYNAMIC;
+    textureDesc.cpuAccessFlags  = CPU_ACCESS_FLAG_WRITE;
+    textureDesc.format          = kTextureFormat;
+    textureDesc.width           = kTextureWidth;
+    textureDesc.height          = kTextureWidth;
+    textureDesc.mipLevels       = 1;
+    ITextureRef texture = mDevice->CreateTexture( textureDesc );
+
+    mContext->BeginScene();
+    {
+        SubResourceData mappedData;
+        mContext->Map( texture, SubResourceIndex(), MAP_TYPE_WRITE, mappedData );
+        {
+            ASSERT_TRUE( mappedData.mem != nullptr );
+            ASSERT_TRUE( mappedData.rowPitch == kTextureWidth * sizeof( uint32_t ) );
+            ASSERT_TRUE( mappedData.slicePitch == kTextureWidth * mappedData.rowPitch );
+
+            uint32_t * ptr = static_cast< uint32_t * >( mappedData.mem );
+            for ( int i = 0, n = kTextureWidth * kTextureWidth; i < n; ++ i )
+            {
+                ptr[ i ] = 0xFF00FF00;
+            }
+        }
+        mContext->Unmap( texture, SubResourceIndex() );
+    }
+    mContext->EndScene();
+
+    IEffectRef effect = mFactory->CreateEffect( mDevice, "draw.fxc" );
+    ConstITechniqueRef technique = effect->GetTechnique( "DrawTexturedQuad" );
+
+    for ( int i = 0; i < 60; ++ i )
+    {
+        Tick();
+
+        mContext->BeginScene();
+        {
+            mContext->SetRenderTargets( 1, &mDefaultRenderTarget );
+            mContext->Clear( mClearParams );
+
+            mContext->SetTexture( SHADER_STAGE_PIXEL, 0, texture );
+
+            technique->Dispatch();
+            mContext->DrawScreenQuad();
+        }
+        mContext->EndScene();
+
+        Present();
+    }
+}
