8793c0b3b1e9a4ffaafe6a42da782fce51658ad6
mode89 <mode89@mail.ru>
2014-09-02 18:39:33 +0400

    gfw pipeline scene collada + building geometry table

--- gfw/source/gfw/pipeline/scene/collada/scene_builder.cpp
+++ gfw/source/gfw/pipeline/scene/collada/scene_builder.cpp
@@ -4,6 +4,7 @@
 #include "gfw/shared/scene_binary.h"
 #include "tinyxml2.h"
 #include <cstring>
+#include <sstream>
 #include <unordered_map>
 
 using namespace tinyxml2;
@@ -40,20 +41,20 @@ namespace GFW {
             XMLElement * xmlLibraryGeometries = xmlCollada->FirstChildElement( "library_geometries" );
             if ( xmlLibraryGeometries )
             {
-                //auto & geometryTable = sceneBinary.geometryTable;
+                auto & geometryTable = sceneBinary.geometryTable;
 
                 for ( XMLElement * xmlGeometry = xmlLibraryGeometries->FirstChildElement( "geometry" );
                     xmlGeometry != nullptr; xmlGeometry = xmlGeometry->NextSiblingElement( "geometry" ) )
                 {
-                    //geometryTable.emplace_back();
-                    //GeometryBinary & geometryBinary = geometryTable.back();
+                    geometryTable.emplace_back();
+                    GeometryBinary & geometryBinary = geometryTable.back();
 
                     for ( XMLElement * xmlGeometryType = xmlGeometry->FirstChildElement();
                         xmlGeometryType != nullptr; xmlGeometryType = xmlGeometryType->NextSiblingElement() )
                     {
                         if ( std::strcmp( xmlGeometryType->Name(), "mesh" ) == 0 )
                         {
-                            //MeshBinary & meshBinary = geometryBinary.mesh;
+                            MeshBinary & meshBinary = geometryBinary.mesh;
 
                             // Collect sources
                             std::unordered_map< std::string, XMLElement * > sourceMap;
@@ -68,10 +69,32 @@ namespace GFW {
                             for ( XMLElement * xmlInput = xmlVertices->FirstChildElement( "input" );
                                 xmlInput != nullptr; xmlInput = xmlInput->NextSiblingElement( "input" ) )
                             {
-                                //const char * semantic = xmlInput->Attribute( "semantic" );
-                                //const char * sourceUri = xmlInput->Attribute( "source" ) + 1;
+                                const char * semanticString = xmlInput->Attribute( "semantic" );
+                                unsigned semanticIndex = xmlInput->IntAttribute( "set" );
+                                const char * sourceUri = xmlInput->Attribute( "source" ) + 1;
 
-                                //XMLElement * xmlSource = sourceMap[ sourceUri ];
+                                // Construct attribute
+
+                                    meshBinary.attributes.emplace_back();
+                                    auto & attribute = meshBinary.attributes.back();
+                                    attribute.semantic = GetSemantic( semanticString, semanticIndex );
+                                    attribute.slot = meshBinary.vertexBuffers.size();
+
+                                    XMLElement * xmlSource = sourceMap[ sourceUri ];
+                                    XMLElement * xmlTechniqueCommon = xmlSource->FirstChildElement( "technique_common" );
+                                    XMLElement * xmlAccessor = xmlTechniqueCommon->FirstChildElement( "accessor" );
+                                    attribute.stride = xmlAccessor->IntAttribute( "stride" );
+
+                                // Construct buffer binary
+
+                                    meshBinary.vertexBuffers.emplace_back();
+                                    auto & buffer = meshBinary.vertexBuffers.back();
+                                    XMLElement * xmlFloatArray = xmlSource->FirstChildElement( "float_array" );
+                                    std::istringstream stream( xmlFloatArray->GetText() );
+                                    std::copy(
+                                        std::istream_iterator< float >( stream ),
+                                        std::istream_iterator< float >(),
+                                        std::back_inserter( buffer ) );
                             }
 
                             XMLElement * xmlTriangles = xmlGeometryType->FirstChildElement( "triangles" );
@@ -79,6 +102,87 @@ namespace GFW {
                                 SceneBuilderException::Unsupported( "Mesh has to be triangulated." ) );
                             CMN_THROW_IF( xmlTriangles->NextSiblingElement( "triangles" ) != nullptr,
                                 SceneBuilderException::Unsupported( "Mesh can have only one set of triangles." ) );
+
+                            XMLElement * xmlIndices = xmlTriangles->FirstChildElement( "p" );
+                            std::vector< unsigned > indices;
+                            std::istringstream indicesStream( xmlIndices->GetText() );
+                            std::copy(
+                                std::istream_iterator< unsigned >( indicesStream ),
+                                std::istream_iterator< unsigned >(),
+                                std::back_inserter( indices ) );
+
+                            unsigned trianglesCount = xmlTriangles->IntAttribute( "count" );
+                            unsigned vertexCount = trianglesCount * 3;
+                            unsigned indexCountPerVertex = indices.size() / vertexCount;
+
+                            // Collect shared inputs
+
+                                for ( XMLElement * xmlInput = xmlTriangles->FirstChildElement( "input" );
+                                    xmlInput != nullptr; xmlInput = xmlInput->NextSiblingElement( "input" ) )
+                                {
+                                    const char * semanticString = xmlInput->Attribute( "semantic" );
+                                    if ( std::strcmp( semanticString, "VERTEX" ) != 0 )
+                                    {
+                                        unsigned semanticIndex = xmlInput->IntAttribute( "set" );
+                                        const char * sourceUri = xmlInput->Attribute( "source" ) + 1;
+
+                                        // Construct attribute
+
+                                            meshBinary.attributes.emplace_back();
+                                            auto & attribute = meshBinary.attributes.back();
+                                            attribute.semantic = GetSemantic( semanticString, semanticIndex );
+                                            attribute.slot = meshBinary.vertexBuffers.size();
+
+                                            XMLElement * xmlSource = sourceMap[ sourceUri ];
+                                            XMLElement * xmlTechniqueCommon = xmlSource->FirstChildElement( "technique_common" );
+                                            XMLElement * xmlAccessor = xmlTechniqueCommon->FirstChildElement( "accessor" );
+                                            attribute.stride = xmlAccessor->IntAttribute( "stride" );
+
+                                        // Construct buffer binary
+
+                                            std::vector< float > sourceBuffer;
+                                            XMLElement * xmlFloatArray = xmlSource->FirstChildElement( "float_array" );
+                                            std::istringstream stream( xmlFloatArray->GetText() );
+                                            std::copy(
+                                                std::istream_iterator< float >( stream ),
+                                                std::istream_iterator< float >(),
+                                                std::back_inserter( sourceBuffer ) );
+
+                                            unsigned indexOffset = xmlInput->IntAttribute( "offset" );
+
+                                            meshBinary.vertexBuffers.emplace_back();
+                                            auto & buffer = meshBinary.vertexBuffers.back();
+                                            buffer.reserve( vertexCount * attribute.stride );
+                                            for ( unsigned i = indexOffset, n = indices.size(); i < n; i += indexCountPerVertex )
+                                            {
+                                                unsigned index = indices[ i ];
+                                                unsigned offset = index * attribute.stride;
+                                                for ( unsigned j = 0; j < attribute.stride; ++ j )
+                                                {
+                                                    buffer.push_back( sourceBuffer[ offset + j ] );
+                                                }
+                                            }
+                                    }
+                                }
+
+                            // Construct index buffer
+
+                                for ( XMLElement * xmlInput = xmlTriangles->FirstChildElement( "input" );
+                                    xmlInput != nullptr; xmlInput = xmlInput->NextSiblingElement( "input" ) )
+                                {
+                                    const char * semanticString = xmlInput->Attribute( "semantic" );
+                                    if ( std::strcmp( semanticString, "VERTEX" ) == 0 )
+                                    {
+                                        unsigned indexOffset = xmlInput->IntAttribute( "offset" );
+                                        auto & buffer = meshBinary.indexBuffer;
+                                        buffer.reserve( vertexCount );
+                                        for ( unsigned i = indexOffset, n = indices.size(); i < n; i += indexCountPerVertex )
+                                        {
+                                            buffer.push_back( indices[ i ] );
+                                        }
+                                        break;
+                                    }
+                                }
                         }
                     }
                 }
