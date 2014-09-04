dfc9479418bb0c5e4dc56daaeee012c01e38026b
mode <mode.andrew@gmail.com>
2014-09-04 19:16:46 +0400

    gfw * refactoring

--- gfw/source/gfw/runtime/ogl4/build.cmake
+++ gfw/source/gfw/runtime/ogl4/build.cmake
@@ -1,3 +1,3 @@
 find_package( opengl CONFIG )
 include_directories( ${OPENGL_INCLUDE_DIRS} )
-target_link_libraries( gfw_runtime ${OPENGL_LIBS} )
+target_link_libraries( gfw-runtime ${OPENGL_LIBS} )
